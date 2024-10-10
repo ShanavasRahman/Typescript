3d95034c677bdb5d6672fb4278a9af1f002a4ccf
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-10-10 20:21:56 +0530

    Practicing typescript

--- script.ts
+++ script.ts
@@ -1,19 +1,39 @@
-type ObjType={
-    firstName: string;
-    lastName: string;
-    age: number;
-    place: string;
+// interface ObjType{
+//     firstName: string;
+//     lastName: string;
+//     age: number;
+//     place: string;
+// }
+
+// let obj: ObjType = {
+//     firstName: "shanavas",
+//     lastName: "Rahman",
+//     age: 25,
+//     place:"Palakkad"
+// }
+
+// for (let key in obj) {
+//     console.log(obj[key as keyof typeof obj]);
+// }
+
+//Accept a char input from the user and display it on the console.
+
+function print(arg:string): void{
+    console.log(arg);
 }
 
-let obj: ObjType = {
-    firstName: "shanavas",
-    lastName: "Rahman",
-    age: 25,
-    place:"Palakkad"
+print("shanavas");
+
+
+function sum(num1: number, num2: number): number{
+    return num1 + num2;
 }
+let result: number = sum(3, 2);
+console.log(result);
 
-for (let key in obj) {
-    console.log(obj[key as keyof typeof obj]);
+function multiply(num: number): void{
+    for (let i = 1; i <= 10; i++){
+        console.log(`${i} x ${num} = ${i * num}`);
+    }
 }
-
-
+multiply(5);
