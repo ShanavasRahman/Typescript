12cf091ef6cde090034fb709afa8869a76a4e9f4
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-10-10 17:45:21 +0530

    Practicing typescript

--- script.ts
+++ script.ts
@@ -1,2 +1,19 @@
+type ObjType={
+    firstName: string;
+    lastName: string;
+    age: number;
+    place: string;
+}
+
+let obj: ObjType = {
+    firstName: "shanavas",
+    lastName: "Rahman",
+    age: 25,
+    place:"Palakkad"
+}
+
+for (let key in obj) {
+    console.log(obj[key as keyof typeof obj]);
+}
 
 
